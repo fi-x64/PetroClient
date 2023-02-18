7cbf117e57b7865586f5500b26fe0005fb267221
fi.x64 <hohoangphia@gmail.com>
2023-02-18 11:03:36 +0700

    Add Search onclick

--- src/services/user.js
+++ src/services/user.js
@@ -1,7 +1,7 @@
 import request from "../utils/request"
 
 export const handleSearchAPI = async (values) => {
-    const res = await request.post('/search', values);
+    const res = await request.post('/user/search', values);
     if (res.data.success) return res.data.data
     return []
 }
